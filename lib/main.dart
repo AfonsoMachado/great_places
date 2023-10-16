e818d54ff2f6bfbe998234b4f5595ff13bf83de4
Afonso Machado <afonsosmachado@gmail.com>
2023-10-16 10:45:28 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/place_form_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:great_places/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.indigo,
+          secondary: Colors.amber
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const PlacesListScreen(),
+      routes: {
+        AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
+      },
+    );
+  }
+}
